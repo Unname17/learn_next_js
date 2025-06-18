ebcd8c09b4cc882a9ee7d99ce9db583a7c3b1e6c
Nama Kamu <emailkamu@example.com>
2025-06-18 22:43:33 +0700

    base learn project

---
+++ lib/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BASE_URL = "http://127.0.0.1:8000/api";
+
+export const login = async (username: string, password: string ) => {
+    const response = await axios.post('${BASE_URL}/login', {
+        username,
+        password,
+    });
+    return response.data;
+};
+
+export const logout = async (token: string | null) => {
+    if (!token) throw new Error("Token tidak ditemukan");
+
+    const res = await axios.post(
+        '${BASE_URL}/logout',
+        {},
+        {
+            headers: {
+                Authorization: 'Barrier ${token}',
+            },
+        }
+    );
+
+    return res.data
+};
